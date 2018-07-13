9d1a0d4fdff163da35b7336e8b7d98724220fb35
Un <keepintine@gmail.com>
2018-07-13 12:58:38 +0300

    Started new class WordList

---
+++ Word Game Prototype/Assets/__Scripts/WordList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordList : MonoBehaviour {
+    public static WordList S;
+
+    public TextAsset wordListText;
+    public int numToParseBeforeYield = 10000;
+    public int wordLengthMin = 3;
+    public int wordLengthMax = 7;
+
+    public bool ________________;
+
+    public int currLine = 0;
+    public int totalLines;
+    public int longWordCount;
+    public int wordCount;
+}
